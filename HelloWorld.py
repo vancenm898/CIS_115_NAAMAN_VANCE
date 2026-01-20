25fac867b2821222c971b8cc61ccfa53e1ede174
SANDHILLS\lh218-02 <jad.talbert@gmail.com>
2026-01-20 09:45:55 -0500

    My frist program mod

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,9 @@
+    #This is my first Python program
 
+print("Hello World")
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+sum = float(num1) + float(num2)
+
+print(sum)
