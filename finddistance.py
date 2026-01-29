f5cee15d1033c14b08714dd9dfe552fba0ad10b7
vancenm898 <vancenm898@mail.sandhills.edu>
2026-01-29 14:57:47 -0500

    yoooooonewdrop

--- finddistance.py
+++ finddistance.py
@@ -1,9 +1,7 @@
 #This progam calculate the distance travelled using the equation x = v * t
 
-velocity = 50 # speed in MPH
+velocity = 80 # speed in MPH
 time = 2 # time in hours
-x = 0 #this is our result variable for distance
+x = float(velocity) * float(time)
 
-x = velocity * time
-
-print(f"{x} mph")
+print(f"{x} miles")
