574eba1334844f72b3d4bde8184b45360783abec
vancenm898 <vancenm898@mail.sandhills.edu>
2026-02-04 12:51:31 -0500

    yooooo

---
+++ sumanddivide.py
@@ -0,0 +1,17 @@
+#This is my 11th Python program
+
+print("Let's divide")
+
+num1 = input("Enter first number: ")
+num2 = input("Enter first number: ")
+num3 = input("Enter first number: ")
+sum = float(num1) + float(num2)
+Quotient = sum / float(num3)
+
+print(Quotient)
+
+if(Quotient % 2 == 0):
+  print("even")
+
+else: 
+  print("odd")
