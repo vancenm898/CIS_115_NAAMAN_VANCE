9c6ad0d8ac31b8296a55d75864ce80dbe9ea39f5
vancenm898 <vancenm898@mail.sandhills.edu>
2026-02-11 12:32:07 -0500

    dude

---
+++ entergrades.py
@@ -0,0 +1,12 @@
+# This program will alow user to enter up to 10m grades
+numofgrades = int(input("How grades would you like to enter?: "))
+Count = 0
+
+if(numofgrades> )
+
+while Count < numofgrades:
+    Count = Count + 1 
+    grades = input("Enter your Grade: ")
+    if(Count >=numofgrades):
+        print("The user entered {numofgrades} grades and is now done")
+
---
+++ myfirstforloop.py
@@ -0,0 +1,5 @@
+
+
+numofiterations = int(input("Enter the number of iterations you would like to uses: "))
+for i in range(numofiterations):
+    print(i)
