f38fa4e0ebf6034d13028b7b531701a8d801d123
vancenm898 <vancenm898@mail.sandhills.edu>
2026-02-28 02:33:02 -0500

    tooooo

--- week7/for_loop_over_list.py
+++ week7/for_loop_over_list.py
@@ -1,5 +1,6 @@
 #use list and for-loops to loop over the list and print it out.
 
+
 def getmylist():
     list = [10,20,30,40,50,60]
     count = 0
@@ -10,4 +11,4 @@ def getmylist():
     
     return count
 total = getmylist()
-print(f"The total count is: {total}")
+print(f"The total loop iterations is: {total}")
