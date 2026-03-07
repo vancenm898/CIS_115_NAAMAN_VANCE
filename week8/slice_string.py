67dfccd37ef4088829cc5a17d2cb734370b49505
vancenm898 <vancenm898@mail.sandhills.edu>
2026-03-07 00:43:56 -0500

    broooooo

--- week8/slice_string.py
+++ week8/slice_string.py
@@ -1,10 +1,10 @@
-# Program should return the postition 0 and 3.(The first and second letter of word.)
+# Program should return the postition 0 and 3.(The first and third letter of word.)
 
 # set varaibles
 word = input("Enter a 4 letters word or more: ")
 
-# This function will print the 1st and second letter of that word.
+# This function will print the 1st and 3rd letter of that word.
 def slice_my_string():
-    print(word[0:3])
+    print(word[0:3:2])
 # This will call the function
 slice_my_string()
