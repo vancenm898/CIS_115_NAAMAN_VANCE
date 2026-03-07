2ace742610899ead176e034a8d4dff6dccd198ec
vancenm898 <vancenm898@mail.sandhills.edu>
2026-03-07 01:41:03 -0500

    moooooo

--- week8/list_append.py
+++ week8/list_append.py
@@ -1,21 +1,30 @@
 #THis program allow user it to create a list of values.
 
+# set a varaible for empty list
 userInputs = []
 
 #function take the varable as an argument, and add to a list
 def  append_to_list(a):
     
+    #adds to list
     userInputs.append(a)
+
+    #prompt users y or n and assins to d (first itiration ends here)
     d = input("Would you like to enter another value to append to the list?(y or n): ")
 
+#while y == d the loop will continue
     while (d == "y" or d =="yes"):
         #Set input varable 
         a = (input("Entervalue: "))
+
         userInputs.append(a)
+
         d = input("Would you like to enter another value to append to the list?(y or n): ")
 
-
+#progam beings here asking for input
 a = (input("Entervalue: "))
+
+# call function
 append_to_list(a)
 
 
