5c2868e2b25b225eafa783ba6738546a2bc20bdb
vancenm898 <vancenm898@mail.sandhills.edu>
2026-04-01 23:41:27 -0400

    bgbgbgb

--- week6/Palindrome.py
+++ week6/Palindrome.py
@@ -1,9 +1,9 @@
 # determine if a word is a palindrome 
-word = input("Give a word: ")
-reverse_word = word [::-1]
-print(reverse_word)
+word = input("Give a word: ") #Takes user input assigns to variable
+reverse_word = word [::-1] # reverses the input and assigms to new variable
+print(reverse_word) # Print new word
 
-if(word == reverse_word):
+if(word == reverse_word): # checks if word reversed is the same if so it prints.
     print(f"The word {word} is a palidrome")
-elif(word != reverse_word):
+elif(word != reverse_word):# checks if word reversed is the same if not it prints.
     print(f"The word {word} is not a palidrome")
